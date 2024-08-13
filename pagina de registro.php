3ecce45d410306cfc7cd7cea5d6cce3eee4c0d8b
brianvillca <136704898+brianvillca@users.noreply.github.com>
2024-08-12 21:57:28 -0300

    aca dejo mi parte del maquetado(en proceso)

---
+++ pagina de registro.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <h1>Pagina13</h1>
+        <div class="topnav">
+            <input type="text" placeholder="ingrese su busqueda">
+        </div>
+
+    </header>
+    <main>
+        <section>
+            <div>
+                <p>Formulario de registro de usuario</p>
+                <p>Correo Electronico</p>
+                <div class="topnav">
+                    <input type="text" placeholder="Ejemplo@gmail.com">
+                </div>
+                <p>Nombre y Apellido</p>
+                <div class="topnav">
+                    <input type="text" placeholder="ingrese tu nombre y apellido">
+                </div>
+                <p>Contraseña</p>
+                <div class="topnav">
+                    <input type="text" placeholder="Crea tu contraseña">
+                </div>
+                <FONT COLOR="gray">La contraseña necesita </FONT><br>
+                <FONT COLOR="gray">Al menos una mayúscula</FONT><br>
+                <FONT COLOR="gray">Al menos un número </FONT><br>
+                <FONT COLOR="gray">Al menos un caracter especial</FONT><br>
+                <FONT COLOR="gray">Al menos una minúscula</FONT><br>
+            </div>
+            <label><input type="checkbox" id="cbox1" value="first_checkbox" /> Recordar contraseña</label><br>
+            <input type="button" value="Registrarse" />
+         </section>
+    </main>
+</body>
+
+</html>
