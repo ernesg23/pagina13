00ebeec9238926accc66ae7c57d22a7ecb1f1004
brianvillca <brianx323@gmail.com>
2024-08-13 12:47:58 -0300

    actualizado

---
+++ views/password.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header>
+        <h1>Pagina13</h1>
+        <div class="searchbar">
+            <input type="text" placeholder="ingrese su busqueda" id="search_article">
+        </div>
+        </header>
+        <main>
+        <section>
+            <div>
+                <p>Formulario de recuperacion de cuenta</p>
+                <p>Ingresa tu Correo Electronico</p>
+                <div class="navbar_password_Recovery">
+                    <input type="text" placeholder="Ejemplo@gmail.com" id="examplebar">
+                </div>
+                <FONT COLOR="gray">Se le enviara un mail con un link que le permitira recuperar su contraseña </FONT><br>
+            </div>
+            <button id="Recovery_password">Recuperar contraseña</button>
+         </section>
+    </main>
+</body>
+</html>
