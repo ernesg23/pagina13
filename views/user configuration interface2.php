880bd6e8bef168aac8dbe9f8eec377fd7f625ae6
benicio vargas <beniciovargas161207@gmail.com>
2024-08-20 10:29:33 -0300

    cambio clases

---
+++ views/user configuration interface2.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="shortcut icon" href="./img/webicon.webp" type="image/x-icon">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina 13</title>
+</head>
+<body>
+    <header>
+        <nav clas="navigation">
+        <div>
+            <h1 class="title">Pagina 13</h1>
+        </div>
+        <search>
+            <form id="bar_nav">
+                <input name="searchBar" id="searchBar" placeholder="Buscar">
+            </form>
+        </search>
+            <input class="User-name" type="button" value="Nombre de usuario">
+            <img class="img" src="./img/descarga1.png">
+        </nav>
+    </header>
+    <main>
+        <section>
+        <img class="img" src="./img/descarga1.png">
+        <h1>Nombre de usuario</h1>
+        <h2>Ejemplo@example.com</h2>
+        <h2>Rol de administrador</h2>
+        <h3>Descripcion de usuario</h3>
+        </section>
+        <section>
+            <div><h1>Configuracion</h1></div>
+            <input class="general" type="button" value="General">
+            <input class="User-admin" type="button" value="Administracion">
+        </section>
+        <section>
+            <h1>Administracion</h1>
+            <h2>Gestor de Articulos</h2>
+            <div>
+                <img class="img" id="big" src="./img/sociedad.jfif">
+                <h2>Titulo</h2>
+                <input type="button" value="tag" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sit optio culpa esse minima dolores dicta. Perspiciatis provident, deleniti asperiores officia omnis cupiditate repudiandae architecto molestias laborum mollitia voluptatum ipsa.</p>
+            </div>
+            <input class="Edit" type="button" value="Editar Articulo">
+            <h2>Eliminar</h2>
+            <form>
+            <input name="searchBar" id="searchBar" placeholder="Razon de Eliminacion">
+            <h6>Este campo es obligatorio*</h6>
+            <input class="Delete" type="button" value="Eliminar">
+        </form>
+        </section>
+        <section>
+            <h1>Articulos Publicados</h1>
+            <div>
+                <img class="img" id="big" src="./img/sociedad.jfif">
+                <h2>Titulo</h2>
+                <input type="button" value="tag" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sit optio culpa esse minima dolores dicta. Perspiciatis provident, deleniti asperiores officia omnis cupiditate repudiandae architecto molestias laborum mollitia voluptatum ipsa.</p>
+            </div>
+        </section>
+        </main>
+</body>
+</html> 
