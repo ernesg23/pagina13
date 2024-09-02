21621607de203c8e5db7e7636a7275977e601730
thiagoMartinez <thiagomartinez960@gmail.com>
2024-09-02 11:17:02 -0300

    Programacion de los botones de redireccionamiento

--- modules/users/register.php
+++ modules/users/register.php
@@ -2,9 +2,12 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
+    <link rel="stylesheet" href="./views/css/home.css">
+    <link rel="shortcut icon" href="./img/webicon.webp" type="image/x-icon">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/registrer.css">
+    <link rel="stylesheet" href="./views/css/register.css">
     <title>Document</title>
 </head>
 
