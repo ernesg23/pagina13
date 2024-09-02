f332b82ad48096df55abf6f581a70d762f980049
s10abbc <latieneroman07@gmail.com>
2024-09-02 10:20:18 -0300

    login terminado bro

--- views/login.php
+++ views/login.php
@@ -1,43 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="./css/styles.css">
     <link rel="shortcut icon" href="./img/logo de empresa.png" type="image/x-icon">
     <link rel="stylesheet" href="./css/login.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina 13</title>
+    <title>Inicio de Sesion de Página 13</title>
 </head>
+
 <body>
-    <header>
-    <nav clas="navigation">
-        <div>
-            <h1 class="tituloPrincipal">Pagina 13</h1>
+    <section>
+
+        <div class="container">
+            <div class="form">
+                <h1 class="title">Página13</h1>
+                <h3 class="title-login">Formulario de inicio de sesion</h3>
+                
+                    <h2 class="title-email">Correo electronico</h2>
+                
+                <form>
+                    <input name="searchBar" id="searchBar" class="search-email" placeholder="ejemplo@example.com">
+                </form>
+                <div class="password">
+                    <h2>Contraseña</h2>
+                </div>
+                <form>
+                    <input name="searchBar" id="searchBar" class="search" placeholder="Introduce tu contraseña">
+                </form>
+
+                <div class="forgotten-password">
+                    <h2>¿Olvidaste tu contraseña?</h2>
+                </div>
+                <div class="remember-password">
+                    <b>
+                        <label><input type="checkbox" id="cbox1" value="first_checkbox" /> Recordar
+                            contraseña</label><br>
+                    </b>
+                </div>
+
+                <input type="button" class="login" value="iniciar sesion" />
+
+            </div>
+
         </div>
-        <search>
-            <form>
-                <input name="searchBar" id="searchBar" placeholder="Buscar">
-            </form>
-        </search>
-            <input class="login" type="button" value="iniciar sesion" />
-            <input class="registrer" type="button" value="registrarse" />
-        </nav>
-    </header>
-        <main>
-            <section>
-            <div class="title-login" ><h1>Formulario de inicio de sesion</h1></div>
-            <h2>Correo electronico</h2>
-            <form>
-                <input name="searchBar" id="searchBar" placeholder="ejemplo@example.com">
-            </form>
-            <h2>Contraseña</h2>
-            <form>
-                <input name="searchBar" id="searchBar" placeholder="Introduce tu contraseña">
-            </form>
-            <h2>¿Olvidaste tu contraseña?</h2>
-            <input class="login" type="button" value="iniciar sesion"/>
-            </section>
-        </main>
+    </section>
 </body>
-</html> 
+
+</html>
