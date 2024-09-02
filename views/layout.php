821bf20f9e2bda4017e23584e3b2d9fd3901be04
benicio vargas <beniciovargas161207@gmail.com>
2024-09-02 11:21:48 -0300

    sigo con el css del index

--- views/layout.php
+++ views/layout.php
@@ -1,21 +1,127 @@
 <html>
 
 <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./css/home.css">
+    <link rel="shortcut icon" href="./img/webicon.webp" type="image/x-icon">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina 13</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script type="module"></script>
 </head>
 
 <body>
+<header>
+        <nav class="navigation">
+        <div>
+            <h1 class="tituloPrincipal">Pagina 13</h1>
+        </div>
+        <search>
+            <form class="searchBar-father">
+                <input class="searchBar" name="searchBar" id="searchBar" placeholder="Buscar">
+                <img class="img-search" id="big" src="./img/busqueda.png">
+            </form>
+        </search>
+        <div class="login-registrer">
+            <input class="login" type="button" value="iniciar sesion"/>
+            <input class="registrer" type="button" value="registrarse"/>
+</div>
+        </nav>
+        <div class="category-parent">
 
-    <div id="menu">
-        <h2>Menu</h2>
-        <a href="#" class="menu-item" data-page="home">home</a><br>
-        <a href="#" class="menu-item" data-page="users-list">usuarios listado</a><br>
-        <a href="#" class="menu-item" data-page="users-create">usuarios alta</a><br>
-    </div>
-
-    <hr>
-
-    <h2>Contenido</h2>
+            <input class="category" type="button" value="categoria 1"/>
+            <input class="category" type="button" value="categoria 2"/>
+            <input class="category" type="button" value="categoria 3"/>
+            <input class="category" type="button" value="categoria 4"/>
+            <input class="category" type="button" value="categoria 5"/>
+            <input class="category" type="button" value="categoria 6"/>
+            <input class="category" type="button" value="categoria 7"/>
+            <input class="category" type="button" value="categoria 8"/>
+            <input class="category" type="button" value="categoria 9"/>
+            <input class="category" type="button" value="categoria 10"/>
+        </div>
+    </header>
+    <main >
+        <section>
+            <div class="article">
+                <div class="big-article">
+                <img class="img" id="big" src="./img/society.jfif">
+                <h2>Titulo</h2>
+                <input type="button" value="categoria" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sit optio culpa esse minima dolores dicta. Perspiciatis provident, deleniti asperiores officia omnis cupiditate repudiandae architecto molestias laborum mollitia voluptatum ipsa.</p>
+                </div>
+                <img class="img" src="./img/society.jfif">
+                <h2>Titulo</h2>
+                <input type="button" value="categoria" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sit optio culpa esse minima dolores dicta. Perspiciatis provident, deleniti asperiores officia omnis cupiditate repudiandae architecto molestias laborum mollitia voluptatum ipsa.</p>
+                <img class="img" src="./img/society.jfif">
+                <h2>Titulo</h2>
+                <input type="button" value="categoria" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sit optio culpa esse minima dolores dicta. Perspiciatis provident, deleniti asperiores officia omnis cupiditate repudiandae architecto molestias laborum mollitia voluptatum ipsa.</p>
+                <img class="img" id="big" src="./img/society.jfif">
+                <h2>Titulo</h2>
+                <input type="button" value="categoria" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sit optio culpa esse minima dolores dicta. Perspiciatis provident, deleniti asperiores officia omnis cupiditate repudiandae architecto molestias laborum mollitia voluptatum ipsa.</p>
+                <img class="img" src="./img/society.jfif">
+                <h2>Titulo</h2>
+                <input type="button" value="categoria" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sit optio culpa esse minima dolores dicta. Perspiciatis provident, deleniti asperiores officia omnis cupiditate repudiandae architecto molestias laborum mollitia voluptatum ipsa.</p>
+                inserte anuncio
+                <h2>Articulos recientes</h2>
+                <img class="img" src="./img/society.jfif">
+                <h2>Titulo</h2>
+                <input type="button" value="categoria" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sit optio culpa esse minima dolores dicta. Perspiciatis provident, deleniti asperiores officia omnis cupiditate repudiandae architecto molestias laborum mollitia voluptatum ipsa.</p>
+                <img class="img" id="big" src="./img/society.jfif">
+                <h2>Titulo</h2>
+                <input type="button" value="categoria" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sit optio culpa esse minima dolores dicta. Perspiciatis provident, deleniti asperiores officia omnis cupiditate repudiandae architecto molestias laborum mollitia voluptatum ipsa.</p>
+                <img class="img" src="./img/society.jfif">
+                <h2>Titulo</h2>
+                <input type="button" value="categoria" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sit optio culpa esse minima dolores dicta. Perspiciatis provident, deleniti asperiores officia omnis cupiditate repudiandae architecto molestias laborum mollitia voluptatum ipsa.</p>
+                <img class="img" src="./img/society.jfif">
+                <h2>Titulo</h2>
+                <input type="button" value="categoria" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sit optio culpa esse minima dolores dicta. Perspiciatis provident, deleniti asperiores officia omnis cupiditate repudiandae architecto molestias laborum mollitia voluptatum ipsa.</p>
+            </div>
+</section>
+<section>
+            <div><h1>Categorias mas visitadas</h1></div>
+            <div>
+            <input class="category" type="button" value="Matematicas"/>
+            <input class="category" type="button" value="Fisica"/>
+            <input class="category" type="button" value="Quimica"/>
+            <input class="category" type="button" value="STEM"/>
+            <input class="category" type="button" value="Pensamiento computacional"/>
+            </div>
+</section>
+        </main>
+    <footer>
+        <div class="inforedes">
+            <h3 class="titles">Explorar</h3>
+            <img class="img" src="./img/logo de empresa.png">
+            <ul>
+                <li><a href="https://twitter.com" class="socialMedia" target="_blank"><i class='bx bxl-twitter' ></i></a></li>
+                <li><a href="https://instagram.com" class="socialMedia" target="_blank"><i class='bx bxl-instagram bx-flip-horizontal' ></i></a></li>
+                <li><a href="https://facebook.com" class="socialMedia" target="_blank"><i class='bx bxl-facebook' ></i></a></li>
+                <li><a href="https://github.com/ho-axed" class="socialMedia" target="_blank"><i class='bx bxl-github' ></i></a></li>
+            </ul>
+        </div>
+        <div>
+            <h4>Articulos publicados hoy</h4>
+            <h5>Titulo Articulo</h5>
+            <h5>Titulo Articulo</h5>
+            <h5>Titulo Articulo</h5>
+        </div>
+        <div>
+            <h4>Temas mas visitados hoy</h4>
+            <h5>Tema 1</h5>
+            <h5>Tema 2</h5>
+            <h5>Tema 3</h5>
+        </div>
+    </footer>
     <div id="content">
         <?php include_once "views/home.php"; ?>
     </div>
