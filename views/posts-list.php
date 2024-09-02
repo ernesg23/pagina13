cc83523d572e8807f2f3a5ac212145adb94366aa
ernesg23 <ernestosecu23@gmail.com>
2024-09-02 13:12:18 -0300

    TAREAS COMPLETADAS: Editar Maquetado Página del Perfil del Usuario; Editar Maquetado Página de Descripción de Artículos; Editar Maquetado Página Artículos-Listado; Editar Maquetado Página de la Configuración del Usuario

--- views/posts-list.php
+++ views/posts-list.php
@@ -1,1 +1,84 @@
-acá va el maquetado del listado
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="shortcut icon" href="./img/webicon.webp" type="image/x-icon">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina 13</title>
+</head>
+
+<body>
+    <header>
+        <!-- Acá va el layout (navbar) -->
+    </header>
+    <main>
+        <section>
+            <h2 id="searchResult"> Artículos encontrados para la búsqueda "hola"</h2>
+            <hr>
+            <div class="articles-container">
+                <div class="article">
+                    <img class="imgPost" src="./img/descarga1.png">
+                    <h3 class="titlePost">Título Artículo</h>
+                        <p class="categoryPost">Categoría</p>
+                        <p class="subtitlePost">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam nisi accusantium nemo hic impedit molestias soluta quaerat eveniet a perferendis! Itaque porro laborum, mollitia minima officiis nihil incidunt eum explicabo!</p>
+                </div>
+                <div class="articles-container">
+                    <div class="article">
+                        <img class="imgPost" src="./img/descarga1.png">
+                        <h3 class="titlePost">Título Artículo</h>
+                            <p class="categoryPost">Categoría</p>
+                            <p class="subtitlePost">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam nisi accusantium nemo hic impedit molestias soluta quaerat eveniet a perferendis! Itaque porro laborum, mollitia minima officiis nihil incidunt eum explicabo!</p>
+                    </div>
+                    <div class="articles-container">
+                        <div class="article">
+                            <img class="imgPost" src="./img/descarga1.png">
+                            <h3 class="titlePost">Título Artículo</h>
+                                <p class="categoryPost">Categoría</p>
+                                <p class="subtitlePost">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam nisi accusantium nemo hic impedit molestias soluta quaerat eveniet a perferendis! Itaque porro laborum, mollitia minima officiis nihil incidunt eum explicabo!</p>
+                        </div>
+                        <div class="articles-container">
+                            <div class="article">
+                                <img class="imgPost" src="./img/descarga1.png">
+                                <h3 class="titlePost">Título Artículo</h>
+                                    <p class="categoryPost">Categoría</p>
+                                    <p class="subtitlePost">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam nisi accusantium nemo hic impedit molestias soluta quaerat eveniet a perferendis! Itaque porro laborum, mollitia minima officiis nihil incidunt eum explicabo!</p>
+                            </div>
+        </section>
+        <section id="multiplePages">
+            <div>
+                <button id="previousPage">Anterior Página</button>
+            </div>
+            <div id="num"></div>
+            <div>
+                <button id="nextPage">Siguiente Página</button>
+            </div>
+        </section>
+        <section>
+            <h2>Artículos Recientes</h2>
+            <div class="articles-container">
+                <div class="article">
+                    <img src="ruta/a/tu/imagen1.jpg" alt="Imagen del artículo 1">
+                    <h3>Título del Artículo 1</h3>
+                    <p>Descripción breve del artículo 1</p>
+                </div>
+                <div class="article">
+                    <img src="ruta/a/tu/imagen2.jpg" alt="Imagen del artículo 2">
+                    <h3>Título del Artículo 2</h3>
+                    <p>Descripción breve del artículo 2</p>
+                </div>
+                <div class="article">
+                    <img src="ruta/a/tu/imagen3.jpg" alt="Imagen del artículo 3">
+                    <h3>Título del Artículo 3</h3>
+                    <p>Descripción breve del artículo 3</p>
+                </div>
+        </section>
+    </main>
+    <footer>
+        <!-- Acá va el layout (footer) -->
+    </footer>
+</body>
+
+</html>
--- views/users-profile.php
+++ views/users-profile.php
@@ -9,29 +9,61 @@
 
 <body>
     <header>
-        <h1>Pagina13</h1>
-        <div id="bar_nav">
-            <input type="text" placeholder="ingrese su busqueda">
-        </div>
-        <p>Nombre de usuario</p>
-        <img src="./img/descarga1.png" loading="lazy" />
+        <!--
+        Acá va el layout (navbar)
+        -->
     </header>
     <section>
         <div>
             <img src="./img/descarga1.png" loading="lazy" />
-            <h1>Nombre de usuario</h1>
-            <p>Correo Electronico del usuario</p>
-            <p>Rol de usuario</p>
-            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAXtJREFUSEu1letRAzEMhDedkEqASgiVAJVAJ4FKgEqAL+PNbGyPIwaiPzfOWdqHdMpGF47NheurCnAj6UEST+JV0lN7LjlWAd4lXXWVALk950AFgMIAEL7/1c5bSR8rkBmA7XhriVhDUIiCRCp6CQBy8jz0YCfpecKo97zvSabcJ0ivwNJhYc9XzQQIQhD4bIOAUnpzsC4BzD6tMDPAsIo7vLei3n9bd1QxA6AorFFBgWxyWnHCtCmBAIECSAw9ePxp4F0r6gnBAhJJgBnBb9jjUU0SkKPOIWZTZJl+5/ORVahKO+nfYO8KwApmALDfB0krOAuANM+9AShkO5BPEVtEn7CtZFFeciIkf9NkiHB/OUWzHUMSjWYAiGx4TtZyTHMFYMV1y6x8aNjGmsDekz70Tc4eJDMz9oeFIveh3yzLVcFlrDB7zvY1x9orxR8lT3IGtZV13Y8kxf60rnvJ2Zt8N9tZQ25FgW3Cc2/Yf//LnKkq/VZVUCo2u/QN4bhvGV+cfPUAAAAASUVORK5CYII=" />
-            <p>configuracion</p>
-            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAORJREFUSEvVlNsNwjAQBDedQCfQCZ0AlVAK0AmdgCeKkXOyjV/54KTIiqLMnvfsnbRxTRvzNUJgJ+m0NHqxDfcKAL9JOizgl6SjE2Sdq1cAuO/eM4HvRwmwg7sk1rCu7mW2q2UHwM7uXyB0GxNpFgg9D/0ORZotsgPFASvCTBjyt0otisHDoa5OTq1ADu5ZD9t56Skqga88r7lo3fDcMR0CTwkMg6cEuJk+W2Jhm/W8ZAbvTIRXwWM7ILi4LN2dp44pAUXO2KruPCXwM34z9kU/2ahgwNTTDZrbydNVpVnULPL/Ah+ORy8Z8tsoHAAAAABJRU5ErkJggg==" />
-            <p>Escribir un articulo</p>
-            <h1>Descripcion de usuario</h1>
+            <h1 class="userName">Nombre de usuario</h1>
+            <p class="userEmail">ejemplo@ejemplo.com.ar</p>
+            <p class="userRol">Rol de Usuario</p>
+            <p>Descripción</p>
+            <p class="userDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores unde consectetur reprehenderit, vero placeat sequi doloremque aspernatur necessitatibus enim nihil odit optio amet cupiditate laboriosam? Necessitatibus atque neque sed iste.</p>
+        </div>
+    </section>
+    <section>
+        <div>
+            <hr>
+            <h2>Artículos Escritos</h2>
+            <div id="writtenPosts_left">
+                <img class="imgPost" src="./img/descarga1.png">
+                <h3 class="titlePost">Título Artículo</h>
+                    <p class="categoryPost">Categoría</p>
+                    <img class="editArticle" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAORJREFUSEvVlNsNwjAQBDedQCfQCZ0AlVAK0AmdgCeKkXOyjV/54KTIiqLMnvfsnbRxTRvzNUJgJ+m0NHqxDfcKAL9JOizgl6SjE2Sdq1cAuO/eM4HvRwmwg7sk1rCu7mW2q2UHwM7uXyB0GxNpFgg9D/0ORZotsgPFASvCTBjyt0otisHDoa5OTq1ADu5ZD9t56Skqga88r7lo3fDcMR0CTwkMg6cEuJk+W2Jhm/W8ZAbvTIRXwWM7ILi4LN2dp44pAUXO2KruPCXwM34z9kU/2ahgwNTTDZrbydNVpVnULPL/Ah+ORy8Z8tsoHAAAAABJRU5ErkJggg==" loading="lazy" />
+                    <p class="subtitlePost">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam nisi accusantium nemo hic impedit molestias soluta quaerat eveniet a perferendis! Itaque porro laborum, mollitia minima officiis nihil incidunt eum explicabo!</p>
+            </div>
+            <div id="writtenPosts_right">
+                <img class="imgPost" src="./img/descarga1.png">
+                <h3 class="titlePost">Título Artículo</h>
+                    <p class="categoryPost">Categoría</p>
+                    <img class="editArticle" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAORJREFUSEvVlNsNwjAQBDedQCfQCZ0AlVAK0AmdgCeKkXOyjV/54KTIiqLMnvfsnbRxTRvzNUJgJ+m0NHqxDfcKAL9JOizgl6SjE2Sdq1cAuO/eM4HvRwmwg7sk1rCu7mW2q2UHwM7uXyB0GxNpFgg9D/0ORZotsgPFASvCTBjyt0otisHDoa5OTq1ADu5ZD9t56Skqga88r7lo3fDcMR0CTwkMg6cEuJk+W2Jhm/W8ZAbvTIRXwWM7ILi4LN2dp44pAUXO2KruPCXwM34z9kU/2ahgwNTTDZrbydNVpVnULPL/Ah+ORy8Z8tsoHAAAAABJRU5ErkJggg==" loading="lazy" />
+                    <p class="subtitlePost">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam nisi accusantium nemo hic impedit molestias soluta quaerat eveniet a perferendis! Itaque porro laborum, mollitia minima officiis nihil incidunt eum explicabo!</p>
+            </div>
+        </div>
+        <div>
+            <h2>Artículos Favoritos</h2>
+            <div id="favoritePosts_left">
+                <img class="imgPost" src="./img/descarga1.png">
+                <h3 class="titlePost">Título Artículo</h>
+                    <p class="categoryPost">Categoría</p>
+                    <p class="subtitlePost">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam nisi accusantium nemo hic impedit molestias soluta quaerat eveniet a perferendis! Itaque porro laborum, mollitia minima officiis nihil incidunt eum explicabo!</p>
+            </div>
+            <div id="favoritePosts_right">
+                <img class="imgPost" src="./img/descarga1.png">
+                <h3 class="titlePost">Título Artículo</h>
+                    <p class="categoryPost">Categoría</p>
+                    <p class="subtitlePost">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam nisi accusantium nemo hic impedit molestias soluta quaerat eveniet a perferendis! Itaque porro laborum, mollitia minima officiis nihil incidunt eum explicabo!</p>
+            </div>
         </div>
 
-        <footer>
 
-        </footer>
+        </div>
+    </section>
+    <footer>
+        <!-- Acá va el layout(footer) -->
+    </footer>
 </body>
 
 </html>
