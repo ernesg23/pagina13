bc346f96181f38dc412d992ae2c444f49250cbdc
thiagoMartinez <thiagomartinez960@gmail.com>
2024-09-02 11:19:51 -0300

    Arreglo login.php

--- modules/users/login.php
+++ modules/users/login.php
@@ -2,9 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="./css/styles.css">
-    <link rel="shortcut icon" href="./img/logo de empresa.png" type="image/x-icon">
-    <link rel="stylesheet" href="./css/login.css">
+    <link rel="stylesheet" href="./views/css/home.css">
+    <link rel="shortcut icon" href="./img/webicon.webp" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./views/css/login.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pagina 13</title>
