0ae3dd3a1952d8e8a3ca154cda07806a6b4f9b9d
thiagoMartinez <thiagomartinez960@gmail.com>
2024-09-03 12:49:31 -0300

    Estilizar Página del Creador de Artículos

--- modules/users/creator.php
+++ modules/users/creator.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./views/css/home.css">
+    <link rel="stylesheet" href="./views/css/creator.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <title>Creador de articulos</title>
 </head>
@@ -12,29 +13,37 @@
 <body>
     <main>
         <section>
-            <h2 id="creatorTitle">Creador de articulos</h2>
-            <div class="creatorContainer">
-                <h3 id="write">Escribir</h3>
-                <h3 id="imagesVideos">Imagenes y videos</h3>
-                <h3 id="sources">Fuentes</h3>
-                <div id="containerPost">
-                    <input type="text" placeholder="Ingrese el titulo del articulo" class="textArea">
-                    <input type="text" placeholder="Ingrese el subtitulo del articulo" class="textArea subtitleTextArea">
-                    <input type="text" placeholder="Ingrese la descripcion del articulo" class="textArea descTextArea">
+            <div class="allContainer">
+
+                <h2 id="creatorTitle">Creador de articulos</h2>
+                <div class="creatorContainer">
+                    <div class="optionsContainer">
+                        <h3 id="write" class="options">Escribir</h3>
+                        <h3 id="imagesVideos" class="options">Imagenes y videos</h3>
+                        <h3 id="sources" class="options">Fuentes</h3>
+                    </div>
+                    <div id="containerPost">
+                        <textarea type="text" placeholder="Ingrese el titulo del articulo" class="textArea"></textarea>
+                        <textarea type="text" placeholder="Ingrese el subtitulo del articulo" id="subtitleTextArea" class="textArea"></textarea>
+                        <textarea type="text" placeholder="Ingrese la descripcion del articulo" id="descTextArea" class="textArea"></textarea>
+                    </div>
+                    <div class="categories">
+                        <div class="categoryCreator">Categoria</div>
+                        <i class='bx bx-plus-medical'></i>
+                        
+                    </div>
+                    <div class="buttonsContainer">
+                        <button id="archiveButton" class="buttonsCreator">Archivar</button>
+                        <button id="sendButton" class="buttonsCreator">Enviar</button>
+                    </div>
                 </div>
-                <div class="categories">
-                    <p class="category">Categoria</p>
-                    <i class='bx bx-plus-medical'></i>
+                <div class="mediaContainer">
+                    <img src="" alt="" loading="lazy" class="imagePostCreator">
+                    <i class='bx bx-plus-medical newImage'></i>
                 </div>
-                <button id="archiveButton">Archivar</button>
-                <button id="sendButtton">Enviar</button>
-            </div>
-            <div class="mediaContainer">
-                <img src="" alt="" loading="lazy" class="imagePostCreator">
-                <i class='bx bx-plus-medical newImage'></i>
-            </div>
-            <div class="sourcesContainer">
-                <input type="text" placeholder="Ingrese la bibliografia" id="sources">
+                <div class="sourcesContainer">
+                    <input type="text" placeholder="Ingrese la bibliografia" id="sources">
+                </div>
             </div>
         </section>
     </main>
