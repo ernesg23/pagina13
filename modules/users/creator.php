2d3449fb73e77bcc645bbc0eb48a9dfd3db905f4
thiagoMartinez <thiagomartinez960@gmail.com>
2024-09-03 11:39:20 -0300

    Editar Maquetado Página del Creador de Artículos

--- modules/users/creator.php
+++ modules/users/creator.php
@@ -5,31 +5,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./views/css/home.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <title>Creador de articulos</title>
 </head>
 
 <body>
     <main>
         <section>
-            <h2>Categoria</h2>
-            <div class="reader">
-                <h3 class="readerTitle">Titulo artículo</h3>
-                <p class="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi fuga hic eos voluptatum modi, voluptatem optio exercitationem numquam pariatur ratione nulla dolorum voluptas ex vero, aliquid, nisi libero. Amet, tempore.</p>
-                <img src="./views/img/society.jfif" alt="" loading="lazy">
-                <div class="author">
-                    <img src="./views/img/profile_default.png" alt="" loading="lazy" class="portraitArticle">
-                    <p id="authorName">Nombre autor</p>
+            <h2 id="creatorTitle">Creador de articulos</h2>
+            <div class="creatorContainer">
+                <h3 id="write">Escribir</h3>
+                <h3 id="imagesVideos">Imagenes y videos</h3>
+                <h3 id="sources">Fuentes</h3>
+                <div id="containerPost">
+                    <input type="text" placeholder="Ingrese el titulo del articulo" class="textArea">
+                    <input type="text" placeholder="Ingrese el subtitulo del articulo" class="textArea subtitleTextArea">
+                    <input type="text" placeholder="Ingrese la descripcion del articulo" class="textArea descTextArea">
                 </div>
-                <div id="description">
-                    <p class="descText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quasi quia fuga nostrum est pariatur natus? Sunt, unde! Libero cumque est enim. Corporis, assumenda! Totam, ipsa esse! Ducimus alias corporis fugiat natus molestias aspernatur quo expedita sequi, dignissimos explicabo praesentium earum quos voluptatibus architecto magnam eum fugit amet modi eos et repudiandae vitae possimus dolor blanditiis. Expedita tempora delectus culpa tempore ratione quasi non ipsa veritatis totam. Expedita unde saepe quibusdam doloremque facere voluptatem repudiandae deleniti aliquid esse debitis! Aperiam, fugiat veritatis quisquam voluptates fugit reiciendis blanditiis, dignissimos culpa beatae harum nulla sed excepturi quos exercitationem accusamus voluptatem et id alias! Tempora non optio nisi, consequatur tempore illum esse enim incidunt nulla vitae reiciendis facilis corrupti mollitia temporibus impedit ratione molestiae unde, labore laboriosam. Possimus enim, explicabo ad natus culpa molestias dolores adipisci rem earum assumenda dolorem nam rerum ipsum vero suscipit, molestiae tempora ea alias praesentium repudiandae nesciunt tempore hic magnam maiores! Hic mollitia dolorum perferendis debitis consequuntur officia, porro, natus repellat dolores amet ex, maiores saepe. Illum, maxime neque tenetur suscipit nesciunt nihil corporis mollitia sed hic eum ipsa saepe similique magni. Dignissimos error sit, voluptatem vel maxime, cumque, doloremque velit consequuntur nobis commodi cupiditate nemo provident tempora fugiat harum labore recusandae. Explicabo reprehenderit ea expedita impedit beatae, nesciunt quaerat exercitationem qui iste cumque voluptates doloremque consequuntur porro dolores eligendi doloribus iusto quam veritatis fugit id. Quod, eos neque perferendis voluptatibus cum aspernatur, quam velit ipsum maxime suscipit temporibus molestias officiis, hic maiores veritatis quisquam labore odio nam? Ratione molestias, accusantium necessitatibus quaerat quod fugit, labore eveniet nulla quae reiciendis ipsam quas, mollitia sint quisquam saepe omnis expedita numquam reprehenderit. Fugit rem repellendus vitae ad sunt non esse exercitationem, minus, perferendis optio nisi. Aliquam, esse fugit dignissimos adipisci aperiam debitis saepe ullam dicta magnam nisi expedita dolor alias magni ducimus facere cupiditate id illum quis at, harum animi hic labore eum. Minima quae temporibus explicabo, laboriosam nostrum accusamus voluptatibus asperiores nihil delectus odit adipisci deserunt quis maxime officiis illo, dicta recusandae unde reprehenderit accusantium. Corporis modi asperiores iure dicta dolorem similique culpa neque labore nostrum veniam! Eos corrupti eius officiis nihil, cupiditate esse excepturi laborum, veniam id earum voluptatum culpa ex? Necessitatibus dicta pariatur blanditiis facere nesciunt illo iste quisquam, molestias nisi, commodi officiis tenetur aspernatur. Voluptate sed ex fuga unde porro iusto corporis at. Dolorum esse dolorem architecto qui, voluptas impedit vitae maxime ipsa aliquam temporibus sit?</p>
+                <div class="categories">
+                    <p class="category">Categoria</p>
+                    <i class='bx bx-plus-medical'></i>
                 </div>
+                <button id="archiveButton">Archivar</button>
+                <button id="sendButtton">Enviar</button>
             </div>
-            <div id="recentArticles">
-                <div class="recent">
-                    <img src="./views/img/society.jfif" alt="" loading="lazy">
-                    <p class="recentTitle">Lorem, ipsum.</p>
-                    <p id="recentSubtitle">Lorem ipsum dolor sit amet.</p>
-                </div>
+            <div class="mediaContainer">
+                <img src="" alt="" loading="lazy" class="imagePostCreator">
+                <i class='bx bx-plus-medical newImage'></i>
+            </div>
+            <div class="sourcesContainer">
+                <input type="text" placeholder="Ingrese la bibliografia" id="sources">
             </div>
         </section>
     </main>
