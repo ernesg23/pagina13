102d6edad4ac3f61d5a80ddc28576f8ef96ff414
s10abbc <latieneroman07@gmail.com>
2024-09-17 10:24:51 -0300

    Avance con Página de configSettings

--- modules/users/configSettings.php
+++ modules/users/configSettings.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="../../views/css/configSettings.css">
     <link rel="shortcut icon" href="./img/webicon.webp" type="image/x-icon">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,29 +15,35 @@
 <body>
     <header>
         <!-- Acá va el layout (navbar) -->
+         <div class="name" <h1>Configuracion</h1> </div>
+        <div class="container"
     </header>
     <main>
         <section>
+            <div class="text"
             <div>
-                <img src="./img/descarga1.png" loading="lazy" />
+                <img src="../../views/img/sin perfil.png" loading="lazy" 
+                width="200"
+                height="200"
+                />
                 <h1 class="userName">Nombre de usuario</h1>
                 <p class="userEmail">ejemplo@ejemplo.com.ar</p>
                 <p class="userRol">Rol de Usuario</p>
                 <p>Descripción</p>
                 <p class="userDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores unde consectetur reprehenderit, vero placeat sequi doloremque aspernatur necessitatibus enim nihil odit optio amet cupiditate laboriosam? Necessitatibus atque neque sed iste.</p>
-                <div>
+</div>
         </section>
-        <section>
+        <section class="nav-config">
             <div>
-                <button class="nav-config" id="users-configSettings">Ajustes</button>
+                <button  id="users-configSettings">Ajustes</button>
             </div>
             <div>
-                <button class="nav-config" id="users-configPosts">Mis Artículos</button>
+                <button  id="users-configPosts">Mis Artículos</button>
             </div>
         </section>
         <section class="contentPage">
             <h1>Ajustes</h1>
-            <h2>Modificar Nombre de Usuario</h2>
+            <h2>Modificar Nombre de Usuario BENI MONO UNGA UNGA</h2>
             <form>
                 <input name="searchBar" id="searchBar_Name" placeholder="Ingrese un nuevo nombre">
             </form>
@@ -57,7 +63,9 @@
                     <button>Guardar</button>
                 </div>
             </div>
+            </div>
         </section>
+        
     </main>
 </body>
 
