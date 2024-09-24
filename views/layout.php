e87dde4201230e349f1ada209123520dbbd3dce7
benicio vargas <beniciovargas161207@gmail.com>
2024-09-24 11:02:21 -0300

    layout movil

--- views/layout.php
+++ views/layout.php
@@ -16,59 +16,71 @@
 
 <body>
     <header>
-            <nav class="navigation">
-                <h1 class="MainTitle">Pagina 13</h1>
-                <search>
-                    <input class="searchBar" name="searchBar" id="searchBar" placeholder="Buscar">
-                </search>
-                <div class="login-register">
-                    <input class="login buttonnav" type="button" value="iniciar sesion" data-page="users-login" />
-                    <input class="registrer buttonnav" type="button" value="registrarse" data-page="users-register" />
-                </div>
-            </nav>
-            <div class="category-parent">
-
-                <input class="category-buttonnav" type="button" value="categoria 1" />
-                <input class="category-buttonnav" type="button" value="categoria 2" />
-                <input class="category-buttonnav" type="button" value="categoria 3" />
-                <input class="category-buttonnav" type="button" value="categoria 4" />
-                <input class="category-buttonnav" type="button" value="categoria 5" />
-                <input class="category-buttonnav" type="button" value="categoria 6" />
-                <input class="category-buttonnav" type="button" value="categoria 7" />
-                <input class="category-buttonnav" type="button" value="categoria 8" />
-                <input class="category-buttonnav" type="button" value="categoria 9" />
-                <input class="category-buttonnav" type="button" value="categoria 10" />
+        <nav class="navigation">
+            <h1 class="MainTitle">Pagina 13</h1>
+            <search>
+                <input class="searchBar" name="searchBar" id="searchBar" placeholder="Buscar">
+            </search>
+            <div class="navMenu">
+                <input class="login buttonnav" type="button" value="iniciar sesion" data-page="users-login" />
+                <input class="registrer buttonnav" type="button" value="registrarse" data-page="users-register" />
             </div>
+        </nav>
+        <div class="category-parent">
+            <input class="category-buttonnav" type="button" value="categoria 1" />
+            <input class="category-buttonnav" type="button" value="categoria 2" />
+            <input class="category-buttonnav" type="button" value="categoria 3" />
+            <input class="category-buttonnav" type="button" value="categoria 4" />
+            <input class="category-buttonnav" type="button" value="categoria 5" />
+            <input class="category-buttonnav" type="button" value="categoria 6" />
+            <input class="category-buttonnav" type="button" value="categoria 7" />
+            <input class="category-buttonnav" type="button" value="categoria 8" />
+            <input class="category-buttonnav" type="button" value="categoria 9" />
+            <input class="category-buttonnav" type="button" value="categoria 10" />
+        </div>
+        <script>
+            const navigationMenu = document.querySelector(".navigationMenu");
+            const navMenu = document.querySelector(".navmenu");
+
+            navigationMenu.addEventListener("click", () => {
+                navigationMenu.classList.toggle("active");
+                navMenu.classList.toggle("active");
+            })
+            document.querySelectorAll(".navlink").forEach(n => n.addEventListener("click", () => {
+                navigationMenu.classList.remove("active");
+                navMenu.classList.remove("active");
+            }))
+        </script>
     </header>
     <div id="content">
         <?php include_once "views/home.php"; ?>
     </div>
     <footer class="footer">
-            <img class="img-foot" src="./views/img/enterprise_logo.png">
-            <div class="social">
-                <ul>
-                    <h4 class="text-title">Seguinos en las redes</h4>
-                    <li><a href="https://twitter.com" class="socialMedia" target="_blank"><i
-                                class='bx bxl-twitter'></i></a><a class="text">Pagina13</a>
-                    </li>
-                    <li><a href="https://instagram.com" class="socialMedia" target="_blank"><i
-                                class='bx bxl-instagram bx-flip-horizontal'></i></a><a class="text">Pagina13</a></li>
-                    <li><a href="https://github.com/ho-axed" class="socialMedia" target="_blank"><i
-                                class='bx bxl-github'></i></a><a class="text">Pagina13</a></li>
-                </ul>
-            </div>
-            <div class="article-today">
-                <h4>Articulos publicados hoy</h4>
-                <h5>Titulo Articulo</h5>
-                <h5>Titulo Articulo</h5>
-                <h5>Titulo Articulo</h5>
-            </div>
-            <div class="public-today">
-                <h4>Temas mas visitados hoy</h4>
-                <h5>Tema 1</h5>
-                <h5>Tema 2</h5>
-                <h5>Tema 3</h5>
-            </div>
+        <img class="img-foot" src="./views/img/enterprise_logo.png">
+        <div class="social">
+            <ul>
+                <h4 class="text-title">Seguinos en las redes</h4>
+                <li><a href="https://twitter.com" class="socialMedia" target="_blank"><i
+                            class='bx bxl-twitter'></i>Pagina13</a>
+                </li>
+                <li><a href="https://instagram.com" class="socialMedia" target="_blank"><i
+                            class='bx bxl-instagram bx-flip-horizontal'></i>Pagina13</a></li>
+                <li><a href="https://github.com/ho-axed" class="socialMedia" target="_blank"><i
+                            class='bx bxl-github'></i>Pagina13</a></li>
+            </ul>
+        </div>
+        <div class="article-today">
+            <h4>Articulos publicados hoy</h4>
+            <h5>Titulo Articulo</h5>
+            <h5>Titulo Articulo</h5>
+            <h5>Titulo Articulo</h5>
+        </div>
+        <div class="public-today">
+            <h4>Temas mas visitados hoy</h4>
+            <h5>Tema 1</h5>
+            <h5>Tema 2</h5>
+            <h5>Tema 3</h5>
+        </div>
     </footer>
 </body>
 
