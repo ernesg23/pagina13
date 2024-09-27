038c8f1c682967867b9a9080e1b9996f27256478
ernesg23 <ernestosecu23@gmail.com>
2024-09-27 02:02:14 -0300

    Estilizar Descripción de Artículos - Complete

--- modules/posts/description.php
+++ modules/posts/description.php
@@ -13,33 +13,37 @@
 <body>
     <main>
         <div id="main-post">
-            <p class="categoryPost">Categoría</p>
-            <div id="writtenPost"></div>
-            <h1 class="titlePost">Título Artículo</h1>
-            <p class="subtitlePost">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam nisi accusantium nemo hic impedit molestias soluta quaerat eveniet a perferendis! Itaque porro laborum, mollitia minima officiis nihil incidunt eum explicabo!</p>
-            <img class="imgPost" src="./img/descarga1.png">
-            <p class="writerName">Nombre Autor</p>
-            <img class="imgWriter" src="">
-            <p class="descriptionPost">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque quia sit qui, harum nobis laudantium, reprehenderit animi temporibus sequi, laborum quos incidunt optio veritatis! Perspiciatis dolorem aspernatur iure similique harum!</p>
+            <p id="categoryPost">Categoría</p>
+            <h2 id="titlePost">Título Artículo</h2>
+            <div id="content">
+                <p id="subtitlePost">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam nisi accusantium nemo hic impedit molestias soluta quaerat eveniet a perferendis! Itaque porro laborum, mollitia minima officiis nihil incidunt eum explicabo!</p>
+                <img id="imgPost" src="../../views/img/society.jfif">
+                <div id="writer">
+                    <img id="imgWriter" src="../../views/img/sin perfil.png">
+                    <p id="nameWriter">Nombre Autor</p>
+                </div>
+                <hr>
+                <p id="descriptionPost">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque quia sit qui, harum nobis laudantium, reprehenderit animi temporibus sequi, laborum quos incidunt optio veritatis! Perspiciatis dolorem aspernatur iure similique harum!</p>
+            </div>
         </div>
         <div id="posts-container">
             <h2>Artículos Recientes</h2>
             <div class="recentPosts">
-                <img src="ruta/a/tu/imagen1.jpg" alt="Imagen del artículo 1">
+                <img src="../../views/img/society.jfif" alt="Imagen del artículo 1">
                 <div>
                     <h3>Título del Artículo 1</h3>
                     <p>Descripción breve del artículo 1</p>
                 </div>
             </div>
             <div class="recentPosts">
-                <img src="ruta/a/tu/imagen2.jpg" alt="Imagen del artículo 2">
+                <img src="../../views/img/society.jfif" alt="Imagen del artículo 2">
                 <div>
                     <h3>Título del Artículo 2</h3>
                     <p>Descripción breve del artículo 2 hhoiijofioj kshifihosiof jshdvsijv</p>
                 </div>
             </div>
             <div class="recentPosts">
-                <img src="ruta/a/tu/imagen3.jpg" alt="Imagen del artículo 3">
+                <img src="../../views/img/society.jfif" alt="Imagen del artículo 3">
                 <div>
                     <h3>Título del Artículo 3</h3>
                     <p>Descripción breve del artículo 3</p>
