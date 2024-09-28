ae64f08b62f0b4c355082dd45bb73180423f09ae
hoaxed <thiagomartinez960@gmail.com>
2024-09-28 18:51:34 -0300

    Arreglando las referencias a archivos

--- modules/users/configSettings.php
+++ modules/users/configSettings.php
@@ -3,13 +3,13 @@
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="../../views/css/configSettings.css">
+    <link rel="stylesheet" href="./views/css/configSettings.css">
     <link rel="shortcut icon" href="./img/webicon.webp" type="image/x-icon">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pagina 13</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="./js/users.js"></script>
+    <script src="./views/js/users.js"></script>
 </head>
 
 <body>
@@ -22,7 +22,7 @@
         <section>
             <div class="text"
             <div>
-                <img src="../../views/img/sin perfil.png" loading="lazy" 
+                <img src="./views/img/sin perfil.png" loading="lazy" 
                 width="200"
                 height="200"
                 />
@@ -57,7 +57,7 @@
             </form>
             <div id="changeImg">
                 <div>
-                    <img class="img" src="./img/descarga1.png">
+                    <img class="img" src="./views/img/descarga1.png">
                 </div>
                 <div>
                     <button>Guardar</button>
--- modules/users/creator.php
+++ modules/users/creator.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./views/css/home.css">
     <link rel="stylesheet" href="./views/css/creator.css">
     <script type="module" src="./views/js/creator.js"></script>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
